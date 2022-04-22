7461beedda29d34f683400b7ef9018268f5e24f0
John <johnathanlee72@hotmail.com>
2022-04-22 13:10:45 -0700

    Initial Commit

---
+++ VideoPractice/VideoPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPracticeApp.swift
+//  VideoPractice
+//
+//  Created by Johnathan Lee on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
